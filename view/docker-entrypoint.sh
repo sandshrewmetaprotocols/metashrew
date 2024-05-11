c40eef9ceb8a48e401ee7ba5e8cf4cea39a7926f
sandshrewmetadev <inquiries@sandshrew.io>
2024-05-11 16:38:52 +0000

    docker-compose

--- view/docker-entrypoint.sh
+++ view/docker-entrypoint.sh
@@ -6,4 +6,4 @@ _PROGRAM_PATH=${PROGRAM_PATH:-/mnt/volume/indexer.wasm}
 echo "DB_LOCATION: $_DB_LOCATION"
 echo "PROGRAM_PATH: $_PROGRAM_PATH"
 
-/view/target/debug/metashrew-view-rs
+/opt/metashrew/target/release/metashrew-view

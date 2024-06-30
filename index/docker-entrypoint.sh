6d4d3471316843004464a25b2f9c46bccd420a70
sandshrew <sandshrewmetaprotocols@gmail.com>
2024-06-30 12:41:51 -0500

    remove monitoring_addr

--- index/docker-entrypoint.sh
+++ index/docker-entrypoint.sh
@@ -8,11 +8,10 @@ _DAEMON_DIR=${DAEMON_DIR:-/bitcoin}
 _DB_DIR=${DB_DIR:-/metashrew/rocksdb}
 _LOG_FILTERS=${LOG_FILTERS:-DEBUG}
 _ELECTRUM_RPC_ADDR=${ELECTRUM_RPC_ADDR:-127.0.0.1:50009}
-_MONITORING_ADDR=${MONITORING_ADDR:-127.0.0.1:34442}
 
 echo "waiting for $_INDEXER to appear ..."
 while [ ! -f $_INDEXER ]; do
   sleep 5
 done
 
-/opt/metashrew/target/release/metashrew --log-filters $_LOG_FILTERS --db-dir $_DB_DIR --daemon-dir $_DAEMON_DIR --network $_NETWORK --daemon-rpc-addr $_DAEMON_RPC_ADDR --daemon-p2p-addr $_DAEMON_P2P_ADDR --electrum-rpc-addr ${_ELECTRUM_RPC_ADDR} --monitoring-addr $_MONITORING_ADDR --indexer $_INDEXER
+/opt/metashrew/target/release/metashrew --log-filters $_LOG_FILTERS --db-dir $_DB_DIR --daemon-dir $_DAEMON_DIR --network $_NETWORK --daemon-rpc-addr $_DAEMON_RPC_ADDR --daemon-p2p-addr $_DAEMON_P2P_ADDR --electrum-rpc-addr ${_ELECTRUM_RPC_ADDR} --indexer $_INDEXER

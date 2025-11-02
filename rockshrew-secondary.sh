0fa1dd3b789ce16fcffe163148355a6899bc3a14
sandshrewmetadev <inquiries@sandshrew.io>
2025-11-02 15:26:39 -0500

    rm secondary

--- rockshrew-secondary.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export RUST_LOG=rockshrew_mono=debug,metashrew_runtime=trace
-ulimit -n $(ulimit -n -H)
-/data/metashrew-v9.0.1/target/release/rockshrew-mono --db-path /data/dbsecondary --daemon-rpc-url http://localhost:8332 --auth bitcoinrpc:bitcoinrpc --indexer /data/alkanes-rs/target/wasm32-unknown-unknown/release/alkanes.wasm --host 0.0.0.0 --port 8081 --start-block 880000
